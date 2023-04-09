81cda79c7847c2cf4e8e7733d27f323d877c3691
drAbdelrahmanGamal <dr.abdo2022@outlook.com>
2023-04-10 01:15:17 +0200

    refactor product service

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
-export class ProductItemDetailComponent {
+export class ProductItemDetailComponent implements OnInit {
+  product: Product = new Product();
+  amountList: number[] = [];
 
+  constructor(
+    private router: ActivatedRoute,
+    private productService: ProductService,
+    private cartService: CartService
+  ) {}
+
+  ngOnInit(): void {
+    this.getProduct();
+    this.amountList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  }
+
+  getProduct(): void {
+    const id: number = Number(this.router.snapshot.paramMap.get('id'));
+    this.productService.getProducts().subscribe((products) => {
+      this.product = products.find((product) => product.id === id)!;
+      this.product.amount = 1;
+    });
+    console.log(this.product);
+  }
+
+  addToCart(): void {
+    this.cartService.addToCart(this.product);
+    alert(
+      `${this.product.amount} items of "${
+        this.product.name
+      }" was added to your cart with price $${
+        this.product.price * this.product.amount
+      }`
+    );
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,31 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, Subscription, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  products: Product[] = [];
-
   constructor(private httpClient: HttpClient) {}
 
   getProducts(): Observable<Product[]> {
     return this.httpClient.get<Product[]>('assets/data.json');
-  }
-
-  getProduct(id: number): Product | null {
-    this.getProducts().subscribe((data) => {
-      data.forEach((product) => {
-        product.amount = 1;
-      });
-      this.products = data;
-    });
-
-    const product: Product | undefined = this.products.find(
-      (product) => product.id === id
-    );
-    return product ? product : null;
   }
 }
